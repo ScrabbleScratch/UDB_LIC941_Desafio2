5fdc50b01f1278f8b8e00df6e1062db6d87e6017
Mario Orellana <ma_oa08@hotmail.com>
2025-06-03 16:44:42 -0600

    change title and icon

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,10 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> 
   </StrictMode>,
 )
