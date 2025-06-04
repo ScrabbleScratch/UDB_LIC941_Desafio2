05c4146c5d25d8b997dd04a128e008646154af32
Mario Orellana <ma_oa08@hotmail.com>
2025-06-03 19:24:05 -0600

    add catalog filters section

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,10 +1,14 @@
-import { Box, Grid } from "@mui/material";
+import { Box, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Select, Stack, Typography } from "@mui/material";
 
-import Products from "../assets/products.json"; // Assuming you have a products.json file in the assets folder
+import Products from "../assets/products.json"; // Assuming we have a products.json file in the assets folder
 
 import ProductCard from "./ProductCard";
+import { useState } from "react";
 
 export default function Catalog() {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+
   return (
     <>
     <Box
@@ -16,12 +20,62 @@ export default function Catalog() {
         objectFit: "cover",
       }}
     />
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      borderBottom="1px solid #e0e0e0"
+    >
+      <Typography variant="h5" p={2}>
+        <b>Productos de la categoría:</b> {category || "Todos"}
+      </Typography>
+      <Stack
+        direction="row"
+        alignItems="center"
+        gap={1}
+        p={2}
+      >
+        <OutlinedInput
+          type="search"
+          placeholder="Buscar"
+          size="small"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <FormControl variant="outlined" size="small" sx={{ minWidth: 200 }}>
+          <InputLabel id="sort-label">Categoría</InputLabel>
+          <Select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            label="Categoría"
+            size="small"
+          >
+            <MenuItem value="">
+              <em>Todos</em>
+            </MenuItem>
+            {Products
+              .map(product => product.category)
+              .filter((value, index, self) => self.indexOf(value) === index)
+              .map((category, index) => (
+                <MenuItem key={index} value={category}>
+                  {category}
+                </MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </Stack>
+    </Stack>
     <Grid container spacing={4} px={20} py={4}>
-      {Products.map((product, index) => (
-        <Grid size={4} key={index}>
-          <ProductCard key={index} product={product} />
-        </Grid>
-      ))}
+      {Products
+        .filter(product => product.category === category || category === "")
+        .filter(product => product.title.toLowerCase().includes(search.toLowerCase()))
+        .map((product, index) => (
+          <Grid size={4} key={index}>
+            <ProductCard key={index} product={product} />
+          </Grid>
+        ))
+      }
     </Grid>
     </>
   );
